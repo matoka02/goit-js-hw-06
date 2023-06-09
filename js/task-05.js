ea46ea6a3b7ba36b828b0c9eb65169e332c3a857
matoka02 <120492521+matoka02@users.noreply.github.com>
2023-06-09 18:29:12 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,8 @@
 "use strict";
 
-// 1. поиск input по dataset
 let inputEl = document.querySelector('#name-input');
-// 2. слушатель input
 inputEl.addEventListener('input', onInputChange);
-// 3. поиск span по dataset
 let outputEl = document.querySelector('#name-output');
-// 4. функция заполнения span
 function onInputChange(event) {
   inputEl = event.currentTarget.value;
   console.log(inputEl);
