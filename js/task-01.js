9b0a5652637004a415be6d338e12fbf1bb5580f2
matoka02 <120492521+matoka02@users.noreply.github.com>
2023-06-09 18:25:35 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,31 +1,8 @@
 "use strict";
 
-// const generalList = Array.from(document.querySelectorAll('ul#categories>li.item'));
-// const generalList = Array.from(document.querySelectorAll('.item'));
-// console.log(`Number of categories: ${generalList.length}`);
-// console.log(generalList);
-
-// Array.prototype.forEach.call(items, (element) => {
-//     const title = element.querySelector('h2').innerHTML;
-//     const itemsLength = element.querySelectorAll('li').length;
-//     console.log(`Категория: ${title} / Количество элементов: ${itemsLength}`);
-// });
-
-// const list = document.querySelectorAll('.item');
-// console.log(`Number of categories: ${list.length}`);
-
-// поиск li в ul с id
-// const subList = document.querySelectorAll('#categories>li');
-// subList.forEach(element => {
-//     const nameSubList = element.firstElementChild.textContent;
-//     const numberSubList = element.lastElementChild.children.length;
-//     console.log(`Category: ${nameSubList} \n Elements: ${numberSubList}`);
-// });
-
-// 1. поиск по классу
 const list = document.querySelectorAll('.item');
 console.log(`Number of categories: ${list.length}`);
-// 2. перебор массива
+
 list.forEach(element => {
     const nameSubList = element.firstElementChild.textContent;
     const numberSubList = element.lastElementChild.children.length;
