8b8bf92825db358e188ff5ff1405001c8bdb76ff
matoka02 <120492521+matoka02@users.noreply.github.com>
2023-06-09 18:30:16 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,10 +1,8 @@
 "use strict";
 
-// 1. поиск span и input по id
 const textEl = document.getElementById("text");
 const inputEl = document.getElementById("font-size-control");
 
-// 2. слушатель
 inputEl.addEventListener("input", (e) => {
     const size = inputEl.value;
     // console.log(size);
