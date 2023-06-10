5cd9292dcaaa746a9609723ef143376edf8648e2
matoka02 <120492521+matoka02@users.noreply.github.com>
2023-06-10 21:24:09 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,6 +1,7 @@
 "use strict";
 
-let valueEl = Number(document.querySelector('#value').textContent);
+// let valueEl = Number(document.querySelector('#value').textContent);
+let valueEl = 0;
 
 let counterValue = document.querySelector('#value');
 console.log(counterValue.textContent);
