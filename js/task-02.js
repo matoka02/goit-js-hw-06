62e2b408beb080b9acef7746ee507d2cf176b686
matoka02 <120492521+matoka02@users.noreply.github.com>
2023-06-10 21:08:13 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -9,13 +9,23 @@ const ingredients = [
   'Condiments',
 ];
 
+// const listRef = document.querySelector('#ingredients');
+
+// const arr = [];
+// for (let i = 0; i < ingredients.length; i++) {
+//   const li = document.createElement('li');
+//   li.classList.add('item');
+//   li.textContent = ingredients[i];
+//   arr.push(li);
+// }
+// listRef.append(...arr);
+
 const listRef = document.querySelector('#ingredients');
 
-const arr = [];
-for (let i = 0; i < ingredients.length; i++) {
-  const li = document.createElement('li');
-  li.classList.add('item');
-  li.textContent = ingredients[i];
-  arr.push(li);
-}
-listRef.append(...arr);
+const markup = ingredients.map((el) =>
+`<li class="item">
+${el}
+</li>`).join('');
+// console.log(markup);
+
+listRef.insertAdjacentHTML('beforeend', markup);
