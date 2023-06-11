87fabfc8dd70ea08716f9a871cb64da18f1c17f3
matoka02 <120492521+matoka02@users.noreply.github.com>
2023-06-11 20:31:31 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,13 +3,14 @@
 let inputEl = document.querySelector('#name-input');
 inputEl.addEventListener('input', onInputChange);
 let outputEl = document.querySelector('#name-output');
+
 function onInputChange(event) {
   outputEl.textContent = event.target.value;
   console.log(outputEl);
 
-//     inputEl !== ' ' ? (outputEl.innerText = inputEl.trim()) : 'Anonymous';
-
+    // inputEl !== ' ' ? (outputEl.innerText = inputEl.trim()) : 'Anonymous';
+  
     if (event.target.value === '') {
       outputEl.textContent = 'Anonymous'
-    }  
-}
+    }
+};
