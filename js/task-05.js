4064f99d5ba4ee071c68568a5b2f49eb9c99ae14
matoka02 <120492521+matoka02@users.noreply.github.com>
2023-06-11 20:25:59 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -4,8 +4,8 @@ let inputEl = document.querySelector('#name-input');
 inputEl.addEventListener('input', onInputChange);
 let outputEl = document.querySelector('#name-output');
 function onInputChange(event) {
-  inputEl = event.currentTarget.value;
-  console.log(inputEl);
+  outputEl.textContent = event.target.value;
+  console.log(outputEl);
 
 //     inputEl !== ' ' ? (outputEl.innerText = inputEl.trim()) : 'Anonymous';
 
