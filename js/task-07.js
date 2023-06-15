2386a67f6ffe76db447c840a6aaca9f675c3f2a4
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-15 22:26:26 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,10 @@
 "use strict";
 
+// 1. поиск span и input по id
 const textEl = document.getElementById("text");
 const inputEl = document.getElementById("font-size-control");
 
+// 2. слушатель
 inputEl.addEventListener("input", (e) => {
     const size = inputEl.value;
     // console.log(size);
