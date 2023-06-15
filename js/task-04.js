1eb8dc7c1aeb2fab7f7aa274ab4ab95c035bec08
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-15 22:26:17 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,14 +1,22 @@
 "use strict";
 
+// 1. поиск содержимого <span>
 // let valueEl = Number(document.querySelector('#value').textContent);
 let valueEl = 0;
 
+// 2. подсчет значений
 let counterValue = document.querySelector('#value');
 console.log(counterValue.textContent);
 
-const dekrBtn = document.querySelector('#counter').firstElementChild;
-const inkrBtn = document.querySelector('#counter').lastElementChild;
+// // 3. поиск button по id div
+// const dekrBtn = document.querySelector('#counter').firstElementChild;
+// const inkrBtn = document.querySelector('#counter').lastElementChild;
 
+// 3. поиск button по dataset
+const dekrBtn = document.querySelector('[data-action="decrement"]');
+const inkrBtn = document.querySelector('[data-action="increment"]');
+
+// 4. слушатели
 inkrBtn.addEventListener('click', event => {
     valueEl += 1;
     counterValue.innerText = valueEl;
@@ -19,3 +27,20 @@ dekrBtn.addEventListener('click', event => {
     // console.log("counterValue", counterValue);
 });
 
+
+// // вариант 
+// let counterValue = 0
+
+// const displayCounter = document.querySelector('#value')
+// const buttonMinus = document.querySelector('button[data-action="decrement"]')
+// const buttonPlus = document.querySelector('button[data-action="increment"]')
+
+// buttonMinus.addEventListener('click', () => {
+//     counterValue--
+//     displayCounter.textContent = counterValue
+// })
+
+// buttonPlus.addEventListener('click', () => {
+//     counterValue++
+//     displayCounter.textContent = counterValue
+// })
