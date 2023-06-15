a37ab97db2f862880b0c852fcb94b2a356aeb3df
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-15 22:26:29 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,11 +1,14 @@
 "use strict";
 
+// 1. поиск по классу
 const form = document.querySelector(".login-form");
-
+// 2. слушатель
 form.addEventListener("submit", handleSubmit);
-
+// 3. функция проверки
 function handleSubmit(event) {
+    // 3.1 запрет перезагрузки
     event.preventDefault();
+    // 3.2 проверка заполненности
     const {
         elements: { email, password }
         } = event.currentTarget;
@@ -13,8 +16,10 @@ function handleSubmit(event) {
     if (email.value === "" || password.value === "") {
     return alert("Please fill in all the fields!");
     }
-
+    // 3.3 сбор объекта
     const userDetails = { email: email.value, password: password.value }
+    // 3.4 показ объекта
     console.log(userDetails);
+    // 3.5 очистка формы
     event.currentTarget.reset();
 }
