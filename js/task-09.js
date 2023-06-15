80539977551f2ed5862627dbfdffb7ce5516c660
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-15 22:26:32 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,17 +1,20 @@
 "use strict";
 
+// 1. события
 const refs = {
   changeButton: document.querySelector('button.change-color'),
   body: document.querySelector('body'),
   colorValue: document.querySelector('span.color')
 };
 
+// 2. слушатели
 refs.changeButton.addEventListener('click', e => {
   let color = getRandomHexColor();
   refs.body.style.backgroundColor = color;
   refs.colorValue.textContent = color;
 });
 
+// 3. функция генерации цвета (была в задании)
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
