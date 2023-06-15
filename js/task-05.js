afdb9bad001b32c49b3bb53a1202ca5b2042a645
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-15 22:26:20 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,13 @@
 "use strict";
 
+// 1. поиск input по dataset
 let inputEl = document.querySelector('#name-input');
+// 2. слушатель input
 inputEl.addEventListener('input', onInputChange);
+// 3. поиск span по dataset
 let outputEl = document.querySelector('#name-output');
+// 4. функция заполнения span
+
 
 function onInputChange(event) {
   outputEl.textContent = event.target.value;
@@ -14,3 +19,15 @@ function onInputChange(event) {
       outputEl.textContent = 'Anonymous'
     }
 };
+
+// // вариант решения
+// const textInput = document.querySelector('#name-input')
+// const output = document.querySelector('#name-output')
+
+// textInput.addEventListener('input', event => {
+//   output.textContent = event.target.value
+
+//   if (event.target.value === '') {
+//     output.textContent = 'Anonymous'
+//   }
+// })
