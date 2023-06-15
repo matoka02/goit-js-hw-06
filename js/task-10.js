dfea5246734267827033bc0e955f962c442cab8b
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-15 22:26:35 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,36 +1,46 @@
 "use strict";
 
+// события
 const refs = {
   render: document.querySelector('[data-create]'),
   destroy: document.querySelector('[data-destroy]'),
   boxes: document.getElementById('boxes'),
 };
 
+// слушатели
 refs.render.addEventListener('click', getAmount);
 refs.destroy.addEventListener('click', destroyBoxes);
 
+// функция получения кол-ва div
 function getAmount() {
   const amount = +document.querySelector('#controls input').value;
   createBoxes(amount);
 };
 
+// функция создания div
 function createBoxes(amount) {
   const basicSize = 30;
   const fragment = document.createDocumentFragment();
 
   for (let i = 0; i < amount; i++) {
+
     const size = basicSize + i * 10;
+
     const div = document.createElement('div');
-    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()} `;    
+
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()} `;
+    
     fragment.appendChild(div);
   }
   refs.boxes.appendChild(fragment);
 }
 
+// функция очищения содержимого div
 function destroyBoxes() {
   refs.boxes.innerHTML = '';
 }
 
+// функция генерации цвета (была в задании)
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
